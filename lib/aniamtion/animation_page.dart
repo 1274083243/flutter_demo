54cfeaaf37632cef569d10aff17de87af6359972
1274083243 <1274083243@qq.com>
2021-12-04 16:13:38 +0800

    隐式动画组件---AnimatedSwitcher

--- lib/aniamtion/animation_page.dart
+++ lib/aniamtion/animation_page.dart
@@ -12,19 +12,26 @@ class _AnimationPageState extends State<AnimationPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: AnimatedContainer(
-        duration: const Duration(seconds: 2),
+      child: Container(
         width: 300,
-        height: 100,
-        decoration: BoxDecoration(
-            gradient: const LinearGradient(
-                colors: [Colors.white, Colors.blue],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                stops: [0.2, 0.3]),
-            borderRadius: BorderRadius.circular(150),
-            border: const Border.fromBorderSide(
-                BorderSide(width: 1, style: BorderStyle.solid))),
+        height: 300,
+        color: Colors.red,
+        child: AnimatedSwitcher(
+          transitionBuilder: (child, animation) {
+            return ScaleTransition(
+                scale: animation,
+                child: FadeTransition(
+                  opacity: animation,
+                  child: child,
+                ));
+          },
+          duration: const Duration(seconds: 2),
+          child: Text(
+            "hellow3",
+            key: UniqueKey(),
+            style: TextStyle(fontSize: 100),
+          ),
+        ),
       ),
     ));
   }
